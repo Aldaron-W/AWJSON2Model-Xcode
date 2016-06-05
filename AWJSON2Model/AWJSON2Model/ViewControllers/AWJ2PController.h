b89d883f75e3c6b24b6e95ca22973bdde0a1e09e
DrizzleWang <wangyu9058@gmail.com>
2016-06-05 21:32:34 +0800

    Add JSON --> Property window controller

---
+++ AWJSON2Model/AWJSON2Model/ViewControllers/AWJ2PController.h
@@ -0,0 +1,13 @@
+//
+//  AWJ2PController.h
+//  AWJSON2Model
+//
+//  Created by Work on 16/6/5.
+//  Copyright © 2016年 Aldaron. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AWJ2PController : NSWindowController
+
+@end
---
+++ AWJSON2Model/AWJSON2Model/ViewControllers/AWJ2PController.m
@@ -0,0 +1,23 @@
+//
+//  AWJ2PController.m
+//  AWJSON2Model
+//
+//  Created by Work on 16/6/5.
+//  Copyright © 2016年 Aldaron. All rights reserved.
+//
+
+#import "AWJ2PController.h"
+
+@interface AWJ2PController ()
+
+@end
+
+@implementation AWJ2PController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
