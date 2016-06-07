4925af22346cf03781d788c68ae6216ea89e9130
Aldaron-W <AldaronWang@gmail.com>
2016-06-07 19:18:02 +0800

    6.7

--- AWJSON2Model/AWJSON2Model/ParseData/ParsePropertyManager.m
+++ AWJSON2Model/AWJSON2Model/ParseData/ParsePropertyManager.m
@@ -9,7 +9,62 @@
 #import <objc/runtime.h>
 #import "ParsePropertyManager.h"
 
+/// Foundation Class Type
+typedef NS_ENUM (NSUInteger, AWNSType) {
+    AWNSTypeNSUnknown = 0,
+    AWNSTypeNSString,
+    AWNSTypeNSMutableString,
+    AWNSTypeNSValue,
+    AWNSTypeNSNumber,
+    AWNSTypeNSDecimalNumber,
+    AWNSTypeNSData,
+    AWNSTypeNSMutableData,
+    AWNSTypeNSDate,
+    AWNSTypeNSURL,
+    AWNSTypeNSArray,
+    AWNSTypeNSMutableArray,
+    AWNSTypeNSDictionary,
+    AWNSTypeNSMutableDictionary,
+    AWNSTypeNSSet,
+    AWNSTypeNSMutableSet,
+};
+
 @implementation ParsePropertyManager
+
++ (AWNSType)getPropertyNSType:(Class)class{
+    
+        if (!class) return AWNSTypeNSUnknown;
+        if ([class isSubclassOfClass:[NSMutableString class]]) return AWNSTypeNSMutableString;
+        if ([class isSubclassOfClass:[NSString class]]) return AWNSTypeNSString;
+        if ([class isSubclassOfClass:[NSDecimalNumber class]]) return AWNSTypeNSDecimalNumber;
+        if ([class isSubclassOfClass:[NSNumber class]]) return AWNSTypeNSNumber;
+        if ([class isSubclassOfClass:[NSValue class]]) return AWNSTypeNSValue;
+        if ([class isSubclassOfClass:[NSMutableData class]]) return AWNSTypeNSMutableData;
+        if ([class isSubclassOfClass:[NSData class]]) return AWNSTypeNSData;
+        if ([class isSubclassOfClass:[NSDate class]]) return AWNSTypeNSDate;
+        if ([class isSubclassOfClass:[NSURL class]]) return AWNSTypeNSURL;
+        if ([class isSubclassOfClass:[NSMutableArray class]]) return AWNSTypeNSMutableArray;
+        if ([class isSubclassOfClass:[NSArray class]]) return AWNSTypeNSArray;
+        if ([class isSubclassOfClass:[NSMutableDictionary class]]) return AWNSTypeNSMutableDictionary;
+        if ([class isSubclassOfClass:[NSDictionary class]]) return AWNSTypeNSDictionary;
+        if ([class isSubclassOfClass:[NSMutableSet class]]) return AWNSTypeNSMutableSet;
+        if ([class isSubclassOfClass:[NSSet class]]) return AWNSTypeNSSet;
+    return AWNSTypeNSUnknown;
+}
+
++ (AWNSType)getPropertyNSTypeForJSON:(Class)class{
+    
+    if (!class) return AWNSTypeNSUnknown;
+    if ([class isSubclassOfClass:[NSMutableString class]]) return AWNSTypeNSMutableString;
+    if ([class isSubclassOfClass:[NSString class]]) return AWNSTypeNSString;
+    if ([class isSubclassOfClass:[NSDecimalNumber class]]) return AWNSTypeNSDecimalNumber;
+    if ([class isSubclassOfClass:[NSNumber class]]) return AWNSTypeNSNumber;
+    if ([class isSubclassOfClass:[NSMutableArray class]]) return AWNSTypeNSMutableArray;
+    if ([class isSubclassOfClass:[NSArray class]]) return AWNSTypeNSArray;
+    if ([class isSubclassOfClass:[NSMutableDictionary class]]) return AWNSTypeNSMutableDictionary;
+    if ([class isSubclassOfClass:[NSDictionary class]]) return AWNSTypeNSDictionary;
+    return AWNSTypeNSUnknown;
+}
 
 /**
  *  格式化OC属性字符串
@@ -21,19 +76,59 @@
  *  @return
  */
 + (NSString *)formatObjcWithKey:(NSString *)key value:(NSObject *)value{
-    NSString *qualifierStr = @"copy";
-    NSString *typeStr = @"NSString";
+    NSString *qualifierStr = @"";
+    NSString *typeStr = @"";
+    
+    switch ([ParsePropertyManager getPropertyNSTypeForJSON:[value class]]) {
+        case AWNSTypeNSString:
+        case AWNSTypeNSMutableString:{
+            qualifierStr = @"copy";
+            typeStr = @"NSString";
+        }
+            break;
+        case AWNSTypeNSNumber:
+        case AWNSTypeNSDecimalNumber:{
+            qualifierStr = @"assign";
+            
+            NSNumber *valueNumber = (NSNumber *)value;
+            if([valueNumber intValue] == [valueNumber longValue]){
+                if ([valueNumber longLongValue] < NSIntegerMax && [valueNumber longLongValue] > NSIntegerMin) {
+                    typeStr = @"NSInteger";
+                }
+                else if ([valueNumber longLongValue] < )
+                
+            }else{
+                if([valueNumber doubleValue] < CGFLOAT_MAX && [valueNumber doubleValue] > CGFLOAT_MIN) {
+                    typeStr = @"CGFloat";
+                }
+                else{
+                    typeStr = @"double";
+                }
+            }
+                
+//            }else{
+//                NSNumber *valueNumber = (NSNumber *)value;
+//                if ([valueNumber longValue]<NSIntegerMax) {
+//                    typeStr = @"NSInteger";
+//                }else{
+//                    typeStr = @"long long";
+//                }
+//            }
+            
+        }
+        default:
+            break;
+    }
     
     //NSString
     if ([value isKindOfClass:[NSString class]]) {
-        return [NSString stringWithFormat:@"@property (nonatomic, %@) %@ *%@;",qualifierStr,typeStr,key];
-        
+        qualifierStr = @"copy";
+        typeStr = @"NSString";
     }else if([value isKindOfClass:[@(YES) class]]){
         //YES or NO
         //the 'NSCFBoolean' is private subclass of 'NSNumber'
         qualifierStr = @"assign";
         typeStr = @"BOOL";
-        return [NSString stringWithFormat:@"@property (nonatomic, %@) %@ %@;",qualifierStr,typeStr,key];
     }else if([value isKindOfClass:[NSNumber class]]){
         //NSNumber
         qualifierStr = @"assign";
@@ -42,7 +137,7 @@
             typeStr = @"CGFloat";
         }else{
             NSNumber *valueNumber = (NSNumber *)value;
-            if ([valueNumber longValue]<2147483648) {
+            if ([valueNumber longValue]<NSIntegerMax) {
                 typeStr = @"NSInteger";
             }else{
                 typeStr = @"long long";
